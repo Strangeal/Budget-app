c8a5777c6ed605c827f89b4b0e6cb005fbb28923
strangeal <bajerijustice@gmail.com>
2023-02-13 05:29:32 -0800

    Add new and create actions in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,24 @@
 class CategoriesController < ApplicationController
   def index; end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.author = current_user
+    if @category.save
+      redirect_to root_path, notice: 'Category Added successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end 
+
 end
