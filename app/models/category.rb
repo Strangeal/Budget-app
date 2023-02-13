f06890ad188dc4b338640f1bde2bd994ea9adb7f
strangeal <bajerijustice@gmail.com>
2023-02-13 01:23:18 -0800

    Add validations for categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :author
   has_many :dealings
+
+  validates :name, presence: true
+  validates :amount, presence: true, comparison: { greater_than: 0 }, numericality: { only_integer: true }
 end
