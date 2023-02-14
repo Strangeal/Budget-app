69b8d51be24029f479bcff5fd3465ff7335faac9
strangeal <bajerijustice@gmail.com>
2023-02-14 13:04:50 -0800

    Add default icon for form

--- db/migrate/20230213101611_create_category.rb
+++ db/migrate/20230213101611_create_category.rb
@@ -3,7 +3,7 @@ class CreateCategory < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.references :author, references: :authors, null: false, foreign_key: {to_table: :users}
       t.string :name
-      t.string :icon
+      t.string :icon, default: "https://cdn-icons-png.flaticon.com/512/4491/4491589.png"
 
       t.timestamps
     end
