ad9abc72e6348d2f0e3fea0cd6703dd879ea94d1
strangeal <bajerijustice@gmail.com>
2023-02-15 03:16:45 -0800

    Remove index action in dealings controller

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,14 +1,4 @@
 class DealingsController < ApplicationController
-
-  def index
-    @category = CategoryDealing.find_by(id: params[:category_id])
-    @category_dealings = Category.includes(:dealings).where(id: @category)
-    @total = 0
-    @category_dealings.each do |u| 
-      u.dealings.each { |z| @total += z.amount } 
-    end
-   end
-
   def new
     @dealing = Dealing.new
     @categories = Category.where(author: current_user)
@@ -19,10 +9,16 @@ class DealingsController < ApplicationController
     @dealing.author = current_user
     if @dealing.save
       CategoryDealing.create(dealing_id: @dealing.id, category_id: params[:category])
-      redirect_to category_dealings_path(id: params[:category_id]), notice: 'Expense added successfully'
+      redirect_to category_path(id: params[:category_id]), notice: 'Expense added successfully'
     else 
       render :new
     end
+  end
+
+  def destroy
+    @dealings = Dealing.find(params[:id])
+    @dealings.destroy
+    redirect_to category_path(id: params[:category_id]), notice: "Transaction successfully deleted"
   end
 
   private
